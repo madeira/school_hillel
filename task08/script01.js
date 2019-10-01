d9c8fb63e9b1271919adc1f894e8eabf746d2cb0
Vladimir Ganziy <v_ganziy@raccoongang.com>
2019-10-01 15:45:25 +0300

    add more style for pretty look

--- task08/script01.js
+++ task08/script01.js
@@ -9,44 +9,37 @@ const url = 'https://trevadim.github.io/vue/data/data.json';
 const body = document.querySelector("body");
 
 let createTitle = (title) => {
-    let strong = document.createElement("strong");
+    let strong = document.createElement("h2");
     strong.innerHTML = title;
+    strong.style.textAlign = "center";
     body.appendChild(strong);
 };
 
 let createInfo = ([info]) => {
     let text = document.createElement("p");
     text.innerHTML = info;
+    text.style.maxWidth = "800px";
+    text.style.margin = "0 auto 40px";
     body.appendChild(text);
 };
 
 let createImg = (img) => {
     let picture = document.createElement("div");
-    picture.innerHTML = `<img src="${img}" alt="">`;
+    picture.innerHTML = `<img src="${img}" style="max-width: 200px; "alt="">`;
+    picture.style.textAlign = "center";
+    picture.style.margin = "0 0 40px";
     body.appendChild(picture);
 };
 
 let createMarkUp = ({planets}) => {
-    console.log(planets);
 
     for (const key in planets) {
-        console.log(key);
         if (planets.hasOwnProperty(key)) {
             createTitle(planets[key].title);
             createImg(planets[key].url);
             createInfo(planets[key].info);
-            
         }
     }
-    
-    
-    // for (const key in planets) {
-    //     if (planets.hasOwnProperty(key)) {
-    //         createTitle(key);
-    //         console.log(key);
-    //     }
-    // }
-    
 };
 
 fetch(url)
